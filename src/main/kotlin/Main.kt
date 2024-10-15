274e439d77e3d5e86b09a51abc0dccb23d931f09
student <mr.nikita954@mail.ru>
2024-10-15 14:07:13 +0300

    second

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,39 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val student:Student? = Student()
+    //let
+    var studentUnit = student?.let{//работает с объектом через it, не возвращает сам объект
+        it.hi()
+        it.bye()
+        it.name = "123"
     }
+    //apply
+    var student2 = student?.apply{//работает с соедржимым объекта, возвравщет объект
+        hi()
+        bye()
+        name = "1234"
+    }
+    //run
+    val stringStudent = Student().run{// рабоатет с содержимым объекта, возвращает последнее значение
+        hi()
+        bye()
+        lastname
+    }
+    println(stringStudent)
+    //with
+    val nameStudent = with(Student()){// рабоатет с содержимым объекта, возвращает последнее значение
+        bye()
+        hi()
+        123
+    }
+    println(nameStudent)
+
+    val StudentCringe = Student().also{//работает с объектом через it, возвращает сам объект
+        it.hi()//ir ot st
+        it.bye()
+        it.name = "Hello"
+        Math.PI
+    }
+    println(StudentCringe.name)
 }
---
+++ src/main/kotlin/student.kt
@@ -0,0 +1,13 @@
+package org.example
+
+class Student {
+    var name:String = "Sasha"
+    val lastname:String = "KEk"
+
+    fun bye(){
+        println("Bye")
+    }
+    fun hi(){
+        println("Hi")
+    }
+}
